f1ba74b7c373eb63ba0019695924380a2ab37313
Tyler <tylerclancy030@gmail.com>
2022-02-10 17:59:00 -0600

    Grab HTML elements

---
+++ public/app.js
@@ -0,0 +1,9 @@
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
