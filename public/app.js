7a383912fe9e6bb0f9d6ed8ffb19944fba476a85
Tyler <tylerclancy030@gmail.com>
2022-02-10 18:52:34 -0600

    Load firebase SDK

--- public/app.js
+++ public/app.js
@@ -7,3 +7,8 @@ const signInBtn = document.getElementById('signInBtn');
 const signOutBtn = document.getElementById('signOutBtn');
 
 const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider(); // Google provider instance
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
