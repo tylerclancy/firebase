39fd9ca43fd5713c85c5c3900345ef4ddf6de137
Tyler <tylerclancy030@gmail.com>
2022-02-11 15:59:46 -0600

    Add button to create document in Firestore

--- public/app.js
+++ public/app.js
@@ -13,17 +13,55 @@ signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
 
+const db = firebase.firestore();
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+let thingsRef; // Reference to a database location
+let unsubscribe; // Turn off realtime stream
+
 auth.onAuthStateChanged(user => {
   if (user) {
     // Signed in
     whenSignedIn.hidden = false;
     whenSignedOut.hidden = true;
     userDetails.innerHTML = `<h3>Greetings, ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+
+    thingsRef = db.collection('things');
+
+    createThing.onclick = () => {
+
+      const { serverTimestamp } = firebase.firestore.FieldValue;
+
+      thingsRef.add({
+        uid: user.uid,
+        name: 'test',
+        createdAt: serverTimestamp()
+      });
+    }
+
+    unsubscribe = thingsRef
+      .where('uid', '==', user.uid)
+      .orderBy('createdAt')
+      .onSnapshot(querySnapshot => {
+        const items = querySnapshot.docs.map(doc => {
+
+          return `<li>${doc.data().name}</li>`
+
+        });
+
+        thingsList.innerHTML = items.join('');
+
+      })
+
   } else {
     // Not signed in
     whenSignedIn.hidden = true;
     whenSignedOut.hidden = false;
     userDetails.innerHTML = '';
+
+    unsubscribe && unsubscribe();
 
   }
 });
